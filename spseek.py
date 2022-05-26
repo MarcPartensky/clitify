2ccca335aa7766504980278d53dbff3198be64c4
Marc Partensky <marc.partensky@gmail.com>
2022-05-27 01:15:50 +0200

    spseek

--- spseek.py
+++ spseek.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
-import spotipy
-from rich import print
 import sys
+import spotipy
 
-duration = int(sys.argv[1])
+duration_s = float(sys.argv[1])
+duration_ms = int(duration_s * 1000)
 
 import termcolor
 
@@ -13,7 +13,9 @@ client_secret = os.environ["SPOTIPY_CLIENT_SECRET"]
 cache_path = os.path.join(os.environ["XDG_RUNTIME_DIR"], ".spcache")
 
 redirect_uri = "http://localhost:8888/callback/"
-scope = "user-read-playback-position user-modify-playback-state"
+scope = (
+    "user-read-playback-position user-read-playback-state user-modify-playback-state"
+)
 
 auth_manager = spotipy.oauth2.SpotifyOAuth(
     client_id=client_id,
@@ -36,10 +38,9 @@ BASE_URL = "https://api.spotify.com/v1/"
 
 
 playback = sp.current_playback()
-print(playback)
 
 progress_ms = playback["progress_ms"]
-position_ms = progress_ms + 1000 * duration
+position_ms = progress_ms + duration_ms
 
 sp.seek_track(position_ms)
 
@@ -51,8 +52,15 @@ name = track["name"]
 artists_names = ", ".join(map(lambda a: a["name"], track["artists"]))
 
 
+if duration_s < 0:
+    duration_text = f"﹣{-duration_s}"
+else:
+    duration_text = f"+{duration_s}"
+
 os.system(f"curl -so /tmp/spimage {image}")
-message = f"+10s {artists_names} - {name}"
+duration_message = termcolor.colored(f"{duration_s}s", "yellow")
+message = f"{duration_text}s {artists_names} - {name}"
 cmd = f'notify-send -i /tmp/spimage "{message}"'
-print(message)
+colored_message = f"{duration_message} {artists_names} - {name}"
+print(colored_message)
 os.system(cmd)

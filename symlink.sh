4db7afa5f66e403a49e83b98a35c9549027f0b7e
Marc Partensky <marc.partensky@gmail.com>
2022-05-27 01:55:38 +0200

    clitify symlink

--- symlink.sh
+++ symlink.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 COUNTER=0
-for script in $(/bin/ls spotipy); do
-    if [[ -x spotipy/$script ]]; then
+folder=clitify
+for script in $(/bin/ls $folder); do
+    if [[ -x $folder/$script ]]; then
         let COUNTER++
         cmd=$(echo $script | cut -d'.' -f1)
-        ln -sf $PWD/spotipy/$script ~/.local/bin/$cmd
+        ln -sf $PWD/$folder/$script ~/.local/bin/$cmd
         echo -e "- \e[1m$script\e[0m as \e[1m$cmd\e[0m"
     fi
 done

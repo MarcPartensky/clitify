7002f9916fceb60ab32ad80106f30d9242afe9f9
Marc Partensky <marc.partensky@gmail.com>
2022-12-16 19:29:59 +0100

    clean

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-COUNTER=0
+
+count=0
 folder=clitify
-for script in $(/bin/ls $folder); do
-    if [[ -x $folder/$script ]]; then
-        let COUNTER++
-        cmd=$(echo $script | cut -d'.' -f1)
+
+for script in `/bin/ls $folder`
+do
+    if [ -x $folder/$script ]
+    then
+        let count++
+        cmd=`echo $script | cut -d'.' -f1`
         ln -sf $PWD/$folder/$script ~/.local/bin/$cmd
         echo -e "- \e[1m$script\e[0m as \e[1m$cmd\e[0m"
     fi
 done
-echo -e "\e[3mLoaded $COUNTER commands\e[0m"
+echo -e "\e[3mLoaded $count commands\e[0m"
